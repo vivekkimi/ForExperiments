c7a65d11689c8468b177a52d04a5016d15784e1d
vivekkimi <vivek.sachidananda@eurecom.fr>
2015-11-29 10:31:54 +0100

    Code Vs Zombies all test passed - Rank 480 Rank_v3 but not pushed before major revamp fo rank

--- Main.py
+++ Main.py
@@ -3,23 +3,30 @@ import sys
 
 # Global variables
 
-
+to_be_saved_human = None
 list_of_hostage = []
 list_of_zombies = []
 
 # Functions
 
 
-def jump_closer_to_hostage():
-        list_of_hostage.sort(key=lambda hostage: hostage.save_rank)
+def jump_closer_to_hostage(james):
+        list_of_hostage.sort(key=lambda hostage: hostage.new_rank_v2)
         debug("closest : " + str(list_of_hostage[0]))
-        print(str(list_of_hostage[0].x) + " " + str(list_of_hostage[0].y))
+        debug("James: " + str(james))
+        debug("to be saved human: " + str(to_be_saved_human))
+        # print(str(list_of_hostage[0].x) + " " + str(list_of_hostage[0].y))
+        if len(list_of_hostage) > 1 and to_be_saved_human and to_be_saved_human.id == list_of_hostage[0].id and james.x == list_of_hostage[0].x and james.y == list_of_hostage[0].y:
+            debug("Already SAVED!!! Mr. " + str(list_of_hostage[0].id))
+            return list_of_hostage[1]
+        return list_of_hostage[0]
 
 
 def jump_closer_to_zombie():
         list_of_zombies.sort(key=lambda zombie: zombie.distance_from_james[1])
-        debug("closest : " + str(list_of_zombies[0]))
-        print(str(list_of_zombies[0].next_x) + " " + str(list_of_zombies[0].next_y))
+        # debug("closest : " + str(list_of_zombies[0]))
+        # print(str(list_of_zombies[0].next_x) + " " + str(list_of_zombies[0].next_y))
+        return list_of_zombies[0]
 
 
 def get_closest_zombie(hostage):
@@ -32,24 +39,32 @@ def get_closest_zombie(hostage):
     return closest_zombie
 
 
-def number_zombies_around(is_current_distance, distance):
+def number_zombies_around(frm_human, is_current_distance, distance):
     no_zombies = 0
     for zombie in list_of_zombies:
         if is_current_distance:
-            if zombie.distance_from_james[0] < distance:
-                no_zombies += 1
+            if frm_human.is_james:
+                compare_distance = zombie.distance_from_james[0]
+            else:
+                compare_distance = zombie.distance_from_hostages[frm_human.id][0]
         else:
-            if zombie.distance_from_james[1] < distance:
-                no_zombies += 1
+            if frm_human.is_james:
+                compare_distance = zombie.distance_from_james[1]
+            else:
+                compare_distance = zombie.distance_from_hostages[frm_human.id][1]
+
+        if compare_distance < distance:
+            no_zombies += 1
+
     return no_zombies
 
 
 def debug(string):
     print(string, file=sys.stderr)
-
+    # return 0
 
 def print_move(james_x, james_y):
-    return debug(str(james_x) + " " + str(james_y))
+    return print(str(james_x) + " " + str(james_y))
 
 # Classes
 
@@ -62,30 +77,65 @@ class Human(object):
         self.distance_from_james = -1
         self.distance_from_closest_zombie = -1
         self.save_rank = 16000
+        # self.to_be_saved_human = None
+        self.new_rank = 16000
+        self.new_rank_v2 = 16000
+        self.is_james = False
 
     def __str__(self):
-        return "Human id: " + str(self.id) + ", (x,y) : " + str(self.x) + "," + str(self.y) + ", rank: " + str(self.save_rank)
+        return_str = "Human id: " + str(self.id) + ", (x,y) : " + str(self.x) + "," + str(self.y) + ", rank: " + str(self.new_rank)
+        # if self.to_be_saved_human:
+        #     return_str + ", to be saved: " + str(self.to_be_saved_human.id)
+        return return_str
 
     def calculate_move(self, nos_zombies, nos_hostages):
-        nos_zmbs_arnd_james = number_zombies_around(False, 400)
+        nos_zmbs_arnd_james = number_zombies_around(self, False, 2000)
+        debug("nos_zmbs_arnd_james : " + str(nos_zmbs_arnd_james))
+        # to_be_saved_human = None
         if nos_zmbs_arnd_james > (nos_zombies / 2):
-            # print_move(self.x, self.y)
-            print("I am waiting!!!")
-        elif (nos_zombies / nos_hostages) >= 1:
-            jump_closer_to_hostage()
+            print_move(self.x, self.y)
+            # print("I am waiting!!!")
+        elif nos_zombies > 1 and  (nos_zombies / nos_hostages) >= 1:
+            to_be_saved_human = jump_closer_to_hostage(self)
+            print_move(to_be_saved_human.x, to_be_saved_human.y)
         else:
-            jump_closer_to_zombie()
+            zombie = jump_closer_to_zombie()
+            print_move(zombie.x, zombie.y)
+        return to_be_saved_human
 
     def calculate_distances(self, mr_james):
         distance_curr_from_james = math.hypot(mr_james.x - self.x, mr_james.y - self.y)
         self.distance_from_james = distance_curr_from_james
-        debug("Hostage : " + str(self.id) + " distance to james: " + str(self.distance_from_james))
+        # debug("Hostage : " + str(self.id) + " distance to james: " + str(self.distance_from_james))
 
-    def calculate_distance_frm_zombie(self, closest_zombie):
+    def calculate_rank(self, closest_zombie):
         distance_from_closest_zombie = math.hypot(closest_zombie.next_x - self.x, closest_zombie.next_y - self.y)
         self.distance_from_closest_zombie = distance_from_closest_zombie
         self.save_rank = self.save_rank - self.distance_from_closest_zombie + self.distance_from_james
 
+        number_of_steps_required_by_zombie = distance_from_closest_zombie/400
+        number_of_steps_required_by_james = self.distance_from_james / 1000
+
+        debug("-------------------------------------")
+        debug(str(self))
+        debug(str(closest_zombie))
+        debug("self.save_rank : " + str(self.save_rank))
+        debug("distance_from_closest_zombie : " + str(distance_from_closest_zombie))
+        debug("distance_from_james : " + str(self.distance_from_james))
+        debug("number_of_steps_required_by_zombie : " + str(number_of_steps_required_by_zombie))
+        debug("number_of_steps_required_by_james: " + str(number_of_steps_required_by_james))
+
+        if number_of_steps_required_by_james < number_of_steps_required_by_zombie:
+            self.new_rank *= (number_of_steps_required_by_james / number_of_steps_required_by_zombie)
+        else:
+            self.new_rank = self.save_rank * self.save_rank
+
+        debug("self.new_rank : " + str(self.new_rank))
+
+        number_of_zombies_around_me = number_zombies_around(self, False, 2000)
+        self.new_rank_v2 = self.new_rank - (number_of_zombies_around_me * 1000)
+        debug("number_of_zombies_around " + str(self.id) + " : " + str(number_of_zombies_around_me) + ", rank: " + str(self.new_rank_v2))
+        debug("************************************************")
 
 class Zombie(object):
     def __init__(self, zombies_id, zombies_x, zombies_y, zombies_next_x, zombies_next_y):
@@ -102,7 +152,7 @@ class Zombie(object):
 
     def calculate_distances(self, mr_james):
         self.distance_from_hostages = {}
-        debug("Zombie: " + str(self.id))
+        # debug("Zombie: " + str(self.id))
         for hostage in list_of_hostage:
             distance_current = math.hypot(hostage.x - self.x, hostage.y - self.y)
             distance_next = math.hypot(hostage.x - self.next_x, hostage.y - self.next_y)
@@ -124,6 +174,7 @@ while 1:
     x, y = [int(i) for i in input().split()]
 
     james_bond = Human("007", x, y)
+    james_bond.is_james = True
 
     human_count = int(input())
 
@@ -139,12 +190,12 @@ while 1:
         list_of_zombies[i].calculate_distances(james_bond)
 
     for i in range(human_count):
-        list_of_hostage[i].calculate_distance_frm_zombie(get_closest_zombie(list_of_hostage[i]))
+        list_of_hostage[i].calculate_rank(get_closest_zombie(list_of_hostage[i]))
 
-    debug("My pos: x: " + str(x) + ", y: " + str(y) + ", human_count: " +  str(human_count) + ", zombies_count: " +  str(zombie_count))
+    debug("My pos: x: " + str(x) + ", y: " + str(y) + ", human_count: " + str(human_count) + ", zombies_count: " + str(zombie_count))
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr)
 
     # Your destination coordinates
-    james_bond.calculate_move(zombie_count, human_count)
+    to_be_saved_human = james_bond.calculate_move(zombie_count, human_count)
